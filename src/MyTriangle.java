4f828983763dc182a89c7a10edfba4b350294851
alu53729679t <alu53729679t@ti2-09>
2018-11-13 19:20:09 +0100

    fotos
    Signed-off-by: alu53729679t <alu53729679t@ti2-09>

---
+++ src/MyTriangle.java
@@ -0,0 +1,15 @@
+import java.awt.Point;
+
+public class MyTriangle extends Point{
+
+	private int v1;
+	private int v2;
+	private int v3;
+	
+	MyTriangle(int x1, int y1, int x2, int y2, int x3, int y3){
+		
+	}
+	public MyTriangle () {
+	
+	}
+}
